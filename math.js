e1bd6bb21303ec2d16c6a5918c83baf3fd05a253
Hosain <cryptichasan925@gmail.com>
2025-04-12 11:47:48 +0600

    vitest run done

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
 export function  add(numbers) {
     let sum = 0;
-    for (const numbers of numbers) {
-        sum += numbers;
+    for (const number of numbers) {
+        sum += number;
     }
     return sum;
 }
