0338f0614b304871391cf6a390253b2b3f43d686
Hosain <cryptichasan925@gmail.com>
2025-04-12 12:26:04 +0600

    use AAA Patern

--- math.test.js
+++ math.test.js
@@ -2,6 +2,30 @@ import {it, expect} from 'vitest';
 import { add } from './math';
 
 it('should return the courrect sum if an array of number is provided', () => {
-    const result = add([1, 2, 3]);
-    expect(result).toBe(6);
-})
+    // Arrange
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce((acc, number) => acc + number, 0);
+    // Action
+    const result = add(numbers);
+    // Assertion
+    expect(result).toBe(expectedResult);
+});
+
+it('should provided NaN if at least one invalid number is probided', () => {
+    // Arrangeconst numbers = [1, "invalid", 3];
+    const numbers = [1, "invalid", 3];
+    // Action
+    const result = add(numbers);
+    // Assertion
+    expect(result).toBeNaN();
+});
+
+it('should return the courrect sum if an array numeric string is provided ', () => {
+    // Arrange
+    const numbers = ["1", 2, 3];
+    const expectedResult = numbers.reduce((acc, cur) => +acc + +cur);
+    // Action
+    const result = add(numbers);
+    // Assertion
+    expect(result).toBe(expectedResult);
+});
